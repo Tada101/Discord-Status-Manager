5b0d40d422e4022c6e740529a6c989163343517c
癌的. Tada <36395101+Tada101@users.noreply.github.com>
2018-02-23 21:27:44 +0100

    Add files via upload

---
+++ Status Manager/StatusMGR.Designer.cs
@@ -0,0 +1,238 @@
+﻿namespace Status_Manager
+{
+    partial class StatusMGR
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(StatusMGR));
+            this.Console = new System.Windows.Forms.RichTextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.supportMeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.discordServerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.githubToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.TokenTXT = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.BTNConnect = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Setgame = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.TwitchURL = new System.Windows.Forms.TextBox();
+            this.checkForUpdatesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Console
+            // 
+            this.Console.BackColor = System.Drawing.SystemColors.Control;
+            this.Console.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.Console.Font = new System.Drawing.Font("Comic Sans MS", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Console.Location = new System.Drawing.Point(-1, 293);
+            this.Console.Name = "Console";
+            this.Console.ReadOnly = true;
+            this.Console.Size = new System.Drawing.Size(563, 71);
+            this.Console.TabIndex = 0;
+            this.Console.Text = "Add me on discord: 癌的. Tada#8195";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.supportMeToolStripMenuItem,
+            this.discordServerToolStripMenuItem,
+            this.githubToolStripMenuItem,
+            this.checkForUpdatesToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(562, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // supportMeToolStripMenuItem
+            // 
+            this.supportMeToolStripMenuItem.Name = "supportMeToolStripMenuItem";
+            this.supportMeToolStripMenuItem.Size = new System.Drawing.Size(81, 20);
+            this.supportMeToolStripMenuItem.Text = "Support me";
+            this.supportMeToolStripMenuItem.Click += new System.EventHandler(this.supportMeToolStripMenuItem_Click);
+            // 
+            // discordServerToolStripMenuItem
+            // 
+            this.discordServerToolStripMenuItem.Name = "discordServerToolStripMenuItem";
+            this.discordServerToolStripMenuItem.Size = new System.Drawing.Size(93, 20);
+            this.discordServerToolStripMenuItem.Text = "Discord server";
+            this.discordServerToolStripMenuItem.Click += new System.EventHandler(this.discordServerToolStripMenuItem_Click);
+            // 
+            // githubToolStripMenuItem
+            // 
+            this.githubToolStripMenuItem.Name = "githubToolStripMenuItem";
+            this.githubToolStripMenuItem.Size = new System.Drawing.Size(55, 20);
+            this.githubToolStripMenuItem.Text = "Github";
+            this.githubToolStripMenuItem.Click += new System.EventHandler(this.githubToolStripMenuItem_Click);
+            // 
+            // TokenTXT
+            // 
+            this.TokenTXT.Location = new System.Drawing.Point(50, 28);
+            this.TokenTXT.Name = "TokenTXT";
+            this.TokenTXT.Size = new System.Drawing.Size(434, 20);
+            this.TokenTXT.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(1, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 18);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Token:";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Black;
+            this.panel1.Location = new System.Drawing.Point(-1, 52);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(581, 2);
+            this.panel1.TabIndex = 4;
+            // 
+            // BTNConnect
+            // 
+            this.BTNConnect.Location = new System.Drawing.Point(490, 28);
+            this.BTNConnect.Name = "BTNConnect";
+            this.BTNConnect.Size = new System.Drawing.Size(60, 20);
+            this.BTNConnect.TabIndex = 5;
+            this.BTNConnect.Text = "Connect";
+            this.BTNConnect.UseVisualStyleBackColor = true;
+            this.BTNConnect.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(4, 82);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(546, 20);
+            this.textBox1.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(1, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 18);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Game:";
+            // 
+            // Setgame
+            // 
+            this.Setgame.FlatAppearance.BorderSize = 0;
+            this.Setgame.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.Setgame.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Setgame.Location = new System.Drawing.Point(462, 259);
+            this.Setgame.Name = "Setgame";
+            this.Setgame.Size = new System.Drawing.Size(88, 28);
+            this.Setgame.TabIndex = 8;
+            this.Setgame.Text = "Set game";
+            this.Setgame.UseVisualStyleBackColor = true;
+            this.Setgame.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(4, 109);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(119, 17);
+            this.checkBox1.TabIndex = 9;
+            this.checkBox1.Text = "Streaming on twitch";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // TwitchURL
+            // 
+            this.TwitchURL.Location = new System.Drawing.Point(130, 107);
+            this.TwitchURL.Name = "TwitchURL";
+            this.TwitchURL.Size = new System.Drawing.Size(420, 20);
+            this.TwitchURL.TabIndex = 10;
+            this.TwitchURL.Visible = false;
+            // 
+            // checkForUpdatesToolStripMenuItem
+            // 
+            this.checkForUpdatesToolStripMenuItem.Name = "checkForUpdatesToolStripMenuItem";
+            this.checkForUpdatesToolStripMenuItem.Size = new System.Drawing.Size(115, 20);
+            this.checkForUpdatesToolStripMenuItem.Text = "Check for updates";
+            this.checkForUpdatesToolStripMenuItem.Click += new System.EventHandler(this.checkForUpdatesToolStripMenuItem_Click);
+            // 
+            // StatusMGR
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(562, 363);
+            this.Controls.Add(this.TwitchURL);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.Setgame);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.BTNConnect);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.TokenTXT);
+            this.Controls.Add(this.Console);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "StatusMGR";
+            this.Opacity = 0.95D;
+            this.Text = "Discord Status Manager";
+            this.Load += new System.EventHandler(this.StatusMGR_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox Console;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem supportMeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem discordServerToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem githubToolStripMenuItem;
+        private System.Windows.Forms.TextBox TokenTXT;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button BTNConnect;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button Setgame;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.TextBox TwitchURL;
+        private System.Windows.Forms.ToolStripMenuItem checkForUpdatesToolStripMenuItem;
+    }
+}
+
---
+++ Status Manager/StatusMGR.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Discord;
+using Discord.WebSocket;
+using Discord.Commands;
+using Discord.Webhook;
+using System.Reflection;
+using System.Diagnostics;
+
+namespace Status_Manager
+{
+    public partial class StatusMGR : Form
+    {
+
+        DiscordSocketClient Client;
+        public static StatusMGR instance = null;
+
+        public StatusMGR()
+        {
+            InitializeComponent();
+            Console.AppendText("\n");
+        }
+
+        private async void StatusMGR_Load(object sender, EventArgs e)
+        {
+            await Task.Delay(1000);
+            Console.AppendText("Discord Status Manager is ready!" + "\n");
+        }
+
+        private void githubToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Process.Start("https://github.com/Tada101");
+        }
+
+        private void discordServerToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Process.Start("https://discord.gg/pSxDtf7");
+        }
+
+        private void supportMeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Process.Start("https://www.paypal.com/pools/c/81TFjR8Pug");
+        }
+        bool connected = false;
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            Client = new DiscordSocketClient(new DiscordSocketConfig()
+            {
+                LogLevel = LogSeverity.Verbose
+            });
+
+            Client.Log += Client_Log;
+
+            try
+            {
+                await Client.LoginAsync(TokenType.User, TokenTXT.Text);
+                await Client.StartAsync();
+                connected = true;
+            }
+            catch
+            {
+                MessageBox.Show("Woops! Is your token a user token?", "Check token!");
+                return;
+            }
+            await Task.Delay(3000);
+        }
+
+        private System.Threading.Tasks.Task Client_Log(LogMessage arg)
+        {
+            Invoke((Action)delegate
+            {
+                Console.AppendText(arg + "\n");
+            });
+            return null;
+        }
+
+        private async void button1_Click_1(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked == true)
+            {
+                if (TwitchURL.Text == "")
+                {
+                    MessageBox.Show("Woops! Please insert a twitch url or disable streaming on twitch", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else
+                {
+                    if (connected = false)
+                    {
+                        MessageBox.Show("Woops! Please connect to a token first", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    else
+                    {
+                        if(checkBox1.Checked == false)
+                        {
+                            await Client.SetGameAsync(textBox1.Text);
+                        }
+                        else
+                        {
+                            await Client.SetGameAsync(textBox1.Text, TwitchURL.Text, StreamType.Twitch);
+                        }
+                    }
+                }
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked == true)
+                TwitchURL.Show();
+            else
+                TwitchURL.Hide();
+        }
+
+        private void checkForUpdatesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Process.Start("https://github.com/Tada101");
+        }
+    }
+}
